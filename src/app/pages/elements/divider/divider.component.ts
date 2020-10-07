178b6892310a63a6d4c73726bfa0e89f404ab9da
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-07 23:25:42 +0100

    #7 | remove unused code

--- src/app/pages/elements/divider/divider.component.ts
+++ src/app/pages/elements/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {Title} from "@angular/platform-browser";
 
 @Component({
@@ -6,14 +6,14 @@ import {Title} from "@angular/platform-browser";
   templateUrl: './divider.component.html',
   styleUrls: ['./divider.component.scss']
 })
-export class DividerComponent implements OnInit {
+export class DividerComponent {
   snippetStandard = `
   <div sui-divider></div>`;
 
   snippetVertical = `
   <div sui-segment>
     <div sui-grid
-         suiRelaxed="true"
+         suiRelaxation="very relaxed"
          suiWidth="two">
       <div suiGridColumn>
         <div sui-placeholder>
@@ -237,74 +237,7 @@ export class DividerComponent implements OnInit {
     </div>
   </div>`;
 
-  stdDivider = `
-  <div>
-    <p> Some content here ...</p>
-  </div>
-
-  <div sui-divider></div>
-
-  <div>
-    <p> Some content here ...</p>
-  </div>`;
-
-  verticalDivider = `
-  <div sui-segment>
-    <div class="ui two column very relaxed grid">
-      <div class="column">
-        <div>
-          <p> Some content ...</p>
-        </div>
-        <div>
-          <p> Some content ...</p>
-        </div>
-      </div>
-
-      <div class="column">
-        <div>
-          <p> Some content ...</p>
-        </div>
-        <div>
-          <p> Some content ...</p>
-        </div>
-      </div>
-    </div>
-    <div sui-divider suiDirection="vertical">
-      and
-    </div>
-  </div>
-      `;
-
-  horizontalDivider = `
-  <div sui-segment suiTextAlignment="center aligned" suiBasic="true">
-    <div sui-input suiAction="left action" suiIcon="true">
-      <input type="text" placeholder="Order #">
-      <div sui-button suiColour="blue">
-        Search
-      </div>
-    </div>
-
-    <div sui-divider suiDirection="horizontal">
-        Or
-    </div>
-
-    <div class="ui teal labeled icon button">
-      Create New Order
-      <i class="add icon"></i>
-    </div>
-  </div>
-  `;
-
   constructor(title: Title) {
     title.setTitle('Dividers | NgxSemantic');
-  }
-
-  toggleStateMap: any = {};
-
-  ngOnInit(): void {
-  }
-
-  toggleLabel(key: string) {
-    this.toggleStateMap[key] = !this.toggleStateMap[key];
   }
 }
