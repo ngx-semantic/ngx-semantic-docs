20e1ba96273efda8357b1602a13cb77dc139607e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-07 23:03:25 +0100

    #7 | add fitted divider support

--- src/app/pages/elements/divider/divider.component.ts
+++ src/app/pages/elements/divider/divider.component.ts
@@ -175,6 +175,17 @@ export class DividerComponent implements OnInit {
     </div>
   </div>`;
 
+  snippetDividerFitted = `
+  <div sui-segment>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam
+    malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+    <div sui-divider
+         suiFitted="true">
+    </div>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam
+    malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+  </div>`;
+
   stdDivider = `
   <div>
     <p> Some content here ...</p>
