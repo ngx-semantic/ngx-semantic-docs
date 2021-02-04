bc70d278a4d0188895703b660332f623ad821c9b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-04 17:33:13 +0100

    #35 | add in component variables

--- src/app/pages/elements/flags/flags.component.ts
+++ src/app/pages/elements/flags/flags.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {HighlightService} from "../../../shared/services/highlight.service";
 
 @Component({
   selector: 'doc-flags',
   templateUrl: './flags.component.html',
   styleUrls: ['./flags.component.scss']
 })
-export class FlagsComponent implements OnInit {
+export class FlagsComponent implements AfterViewChecked {
+  isDefinitionsActive = true;
+  isApiActive: boolean;
+
   snippetFlag = `
   <i suiCountry="uae" sui-flag></i>
   <i suiCountry="burkina faso" sui-flag></i>
@@ -13,9 +18,23 @@ export class FlagsComponent implements OnInit {
   <i suiCountry="nigeria" sui-flag></i>
   `;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Flags | Ngx Semantic');
   }
 
+  ngAfterViewChecked() {
+    this.highlighter.highlightAll();
+  }
+
+  tabChanged(payload: string): void {
+    if (payload === 'api') {
+      this.isApiActive = true;
+      this.isDefinitionsActive = false;
+    }
+
+    if (payload === 'definitions') {
+      this.isApiActive = false;
+      this.isDefinitionsActive = true;
+    }
+  }
 }
