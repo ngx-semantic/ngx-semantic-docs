7a3b2c966a5a39c4f3872827c5c1c005effc81ea
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-06 08:41:30 +0100

    #18 | add in component variabbles

--- src/app/pages/elements/headers/headers.component.ts
+++ src/app/pages/elements/headers/headers.component.ts
@@ -1,30 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {HighlightService} from "../../../shared/services/highlight.service";
 
 @Component({
   selector: 'doc-headers',
   templateUrl: './headers.component.html',
   styleUrls: ['./headers.component.scss']
 })
-export class HeadersComponent implements OnInit {
+export class HeadersComponent implements AfterViewChecked {
+  isDefinitionsActive = true;
+  isApiActive: boolean;
+
   snippetColored = `
   <div class="ui segment">
     <h4 sui-header suiColour="red" suiInverted>Red</h4>
     <h4 sui-header suiColour="orange" suiInverted>Orange</h4>
     <h4 sui-header suiColour="yellow" suiInverted>Yellow</h4>
     <h4 sui-header suiColour="olive" suiInverted>Olive</h4>
-  </div>
-  `;
+  </div>`;
+
   snippetInverted = `
   <div class="ui inverted segment">
     <h4 sui-header suiColour="teal" suiInverted>Teal</h4>
     <h4 sui-header suiColour="blue" suiInverted>Blue</h4>
     <h4 sui-header suiColour="purple" suiInverted>Purple</h4>
     <h4 sui-header suiColour="violet" suiInverted>Violet</h4>
-  </div>
-  `;
+  </div>`;
 
-  constructor() { }
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Dividers | Ngx Semantic');
+  }
 
-  ngOnInit(): void {
+  ngAfterViewChecked() {
+    this.highlighter.highlightAll();
+  }
+
+  tabChanged(payload: string): void {
+    if (payload === 'api') {
+      this.isApiActive = true;
+      this.isDefinitionsActive = false;
+    }
+
+    if (payload === 'definitions') {
+      this.isApiActive = false;
+      this.isDefinitionsActive = true;
+    }
   }
 }
