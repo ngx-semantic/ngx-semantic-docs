0bf5b7196e1a134f4203c19d4ca2b8c7fe4a7655
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-09 08:11:07 +0100

    #19 | add in the disabled and loading icons

--- src/app/pages/elements/icons/icons.component.ts
+++ src/app/pages/elements/icons/icons.component.ts
@@ -11,6 +11,22 @@ export class IconsComponent implements AfterViewChecked {
   isDefinitionsActive = true;
   isApiActive: boolean;
 
+  snippetIconDisabled = `
+  <i sui-icon
+     disabled
+     suiIconType="users"></i>`;
+
+  snippetIconLoading = `
+  <i sui-icon
+     suiLoading
+     suiIconType="spinner"></i>
+  <i sui-icon
+     suiLoading
+     suiIconType="notched circle"></i>
+  <i sui-icon
+     suiLoading
+     suiIconType="asterisk"></i>`;
+
   constructor(title: Title, private highlighter: HighlightService) {
     title.setTitle('Icons | Ngx Semantic');
   }
