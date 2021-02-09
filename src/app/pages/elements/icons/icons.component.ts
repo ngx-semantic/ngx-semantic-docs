4945d88f3b87955a775beb5bc20fa39a05826888
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-09 08:03:26 +0100

    #19 | add component variables

--- src/app/pages/elements/icons/icons.component.ts
+++ src/app/pages/elements/icons/icons.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {HighlightService} from "../../../shared/services/highlight.service";
 
 @Component({
   selector: 'doc-icons',
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.scss']
 })
-export class IconsComponent implements OnInit {
+export class IconsComponent implements AfterViewChecked {
+  isDefinitionsActive = true;
+  isApiActive: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Icons | Ngx Semantic');
   }
 
+  ngAfterViewChecked() {
+    this.highlighter.highlightAll();
+  }
+
+  tabChanged(payload: string): void {
+    if (payload === 'api') {
+      this.isApiActive = true;
+      this.isDefinitionsActive = false;
+    }
+
+    if (payload === 'definitions') {
+      this.isApiActive = false;
+      this.isDefinitionsActive = true;
+    }
+  }
 }
