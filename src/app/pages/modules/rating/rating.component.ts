049824d2e6d1441f45c582d3086e7c62a3897989
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-11 10:42:39 +0100

    #49 | add in page variables

--- src/app/pages/modules/rating/rating.component.ts
+++ src/app/pages/modules/rating/rating.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {HighlightService} from "../../../shared/services/highlight.service";
 
 @Component({
   selector: 'doc-rating',
-  template: `
-    <p>
-      rating works!
-    </p>
-  `,
+  templateUrl: './rating.component.html',
   styles: [
   ]
 })
-export class RatingComponent implements OnInit {
+export class RatingComponent implements AfterViewChecked {
+  isDefinitionsActive = true;
+  isApiActive: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Rating | Ngx Semantic');
   }
 
+  ngAfterViewChecked() {
+    this.highlighter.highlightAll();
+  }
+
+  tabChanged(payload: string): void {
+    if (payload === 'api') {
+      this.isApiActive = true;
+      this.isDefinitionsActive = false;
+    }
+
+    if (payload === 'definitions') {
+      this.isApiActive = false;
+      this.isDefinitionsActive = true;
+    }
+  }
 }
