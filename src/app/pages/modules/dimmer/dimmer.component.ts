6f3bac7bb8c3ab4a956b8c6b7e2b910d3f47e463
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-19 10:31:34 +0100

    #43 | add the dimmer scaffold

--- src/app/pages/modules/dimmer/dimmer.component.ts
+++ src/app/pages/modules/dimmer/dimmer.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {HighlightService} from "../../../shared/services/highlight.service";
 
 @Component({
   selector: 'doc-dimmer',
-  template: `
-    <p>
-      dimmer works!
-    </p>
-  `,
+  templateUrl: './dimmer.component.html',
   styles: [
   ]
 })
-export class DimmerComponent implements OnInit {
+export class DimmerComponent implements AfterViewChecked {
+  isDefinitionsActive = true;
+  isApiActive: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Tab | Ngx Semantic');
   }
 
+  ngAfterViewChecked() {
+    this.highlighter.highlightAll();
+  }
+
+  tabChanged(payload: string): void {
+    if (payload === 'api') {
+      this.isApiActive = true;
+      this.isDefinitionsActive = false;
+    }
+
+    if (payload === 'definitions') {
+      this.isApiActive = false;
+      this.isDefinitionsActive = true;
+    }
+  }
 }
