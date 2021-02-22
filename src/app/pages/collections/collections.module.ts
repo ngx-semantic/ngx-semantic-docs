251da14f08bcb2c1659af7c61442d03244e16519
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-22 16:04:05 +0100

    #79 | update packages

--- src/app/pages/collections/collections.module.ts
+++ src/app/pages/collections/collections.module.ts
@@ -3,31 +3,35 @@
  */
 
 import {NgModule} from "@angular/core";
-import {BreadcrumbComponent} from "./breadcrumb/breadcrumb.component";
+import {CommonModule} from "@angular/common";
 import {FormComponent} from "./form/form.component";
 import {GridComponent} from "./grid/grid.component";
 import {MenuComponent} from "./menu/menu.component";
-import {MessagesComponent} from "./messages/messages.component";
 import {TableComponent} from "./table/table.component";
-import {CommonModule} from "@angular/common";
-import {CollectionsRoutingModule} from "./collections-routing.module";
 import {SharedModule} from "../../shared/shared.module";
-import {NgxSemanticModule} from "ngx-semantic";
+import {MessagesComponent} from "./messages/messages.component";
+import {BreadcrumbComponent} from "./breadcrumb/breadcrumb.component";
+import {CollectionsRoutingModule} from "./collections-routing.module";
+import {SuiBreadcrumbModule, SuiFormModule, SuiHeaderModule, SuiIconModule, SuiMessageModule} from "ngx-semantic";
 
 @NgModule({
   declarations: [
-    BreadcrumbComponent,
     FormComponent,
     GridComponent,
     MenuComponent,
+    TableComponent,
     MessagesComponent,
-    TableComponent
+    BreadcrumbComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
-    NgxSemanticModule,
-    CollectionsRoutingModule
+    SuiFormModule,
+    SuiIconModule,
+    SuiHeaderModule,
+    SuiMessageModule,
+    CollectionsRoutingModule,
+    SuiBreadcrumbModule
   ]
 })
 export class CollectionsModule {
