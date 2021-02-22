88294bc3dc5b3b09b28ab527e6f30a7c9c38f64c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-22 16:21:28 +0100

    #79 | update breadcrumb content

--- src/app/pages/collections/breadcrumb/breadcrumb.component.ts
+++ src/app/pages/collections/breadcrumb/breadcrumb.component.ts
@@ -17,16 +17,16 @@ export class BreadcrumbComponent implements AfterViewChecked {
       <div suiBreadcrumbDivider> / </div>
       <a suiBreadcrumbSection>Store</a>
       <div suiBreadcrumbDivider> / </div>
-      <div suiBreadcrumbSection suiActive='true'>T-Shirt</div>
+      <div suiBreadcrumbSection suiActive>T-Shirt</div>
   </div>
   `;
   snippetStd1 = `
-  <div sui-breadcrumb suiIcon='true'>
+  <div sui-breadcrumb>
       <a suiBreadcrumbSection>Home</a>
       <i suiIconType="right angle" sui-icon suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Store</a>
       <i suiIconType="right angle" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>T-Shirt</div>
+      <div suiBreadcrumbSection suiActive>T-Shirt</div>
   </div>
   `;
   snippetContent1 = `
@@ -35,22 +35,22 @@ export class BreadcrumbComponent implements AfterViewChecked {
       <div suiBreadcrumbDivider> / </div>
       <a suiBreadcrumbSection>Registration</a>
       <div suiBreadcrumbDivider> / </div>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetContent2 = `
-  <div sui-breadcrumb suiIcon='true'>
+  <div sui-breadcrumb>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
 
   snippetSection = `
   <div sui-breadcrumb>
       <span suiBreadcrumbSection>Home</span>
       <div suiBreadcrumbDivider> / </div>
-      <div suiBreadcrumbSection suiActive='true'>Search</div>
+      <div suiBreadcrumbSection suiActive>Search</div>
   </div>
   `;
 
@@ -58,7 +58,7 @@ export class BreadcrumbComponent implements AfterViewChecked {
   <div sui-breadcrumb>
       <a suiBreadcrumbSection>Home</a>
       <div suiBreadcrumbDivider> / </div>
-      <div suiBreadcrumbSection suiActive='true'>Search for: <a href="#">paper towels</a></div>
+      <div suiBreadcrumbSection suiActive>Search for: <a href="#">paper towels</a></div>
   </div>
   `;
 
@@ -66,65 +66,65 @@ export class BreadcrumbComponent implements AfterViewChecked {
   <div sui-breadcrumb>
       <a suiBreadcrumbSection>Products</a>
       <div suiBreadcrumbDivider> / </div>
-      <div suiBreadcrumbSection suiActive='true'>Paper Towels</div>
+      <div suiBreadcrumbSection suiActive>Paper Towels</div>
   </div>
   `;
 
   snippetSize = `
-  <div sui-breadcrumb suiIcon='true' suiSize='mini'>
+  <div sui-breadcrumb suiSize='mini'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetSize1 = `
-  <div sui-breadcrumb suiIcon='true' suiSize='tiny'>
+  <div sui-breadcrumb suiSize='tiny'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetSize2 = `
-  <div sui-breadcrumb suiIcon='true' suiSize='small'>
+  <div sui-breadcrumb suiSize='small'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetSize3 = `
-  <div sui-breadcrumb suiIcon='true' suiSize='large'>
+  <div sui-breadcrumb suiSize='large'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetSize4 = `
-  <div sui-breadcrumb suiIcon='true' suiSize='big'>
+  <div sui-breadcrumb suiSize='big'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetSize5 = `
-  <div sui-breadcrumb suiIcon='true' suiSize='huge'>
+  <div sui-breadcrumb suiSize='huge'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
   snippetSize6 = `
-  <div sui-breadcrumb suiIcon='true' suiSize='massive'>
+  <div sui-breadcrumb suiSize='massive'>
       <a suiBreadcrumbSection>Home</a>
-      <i suiIconType="right chevron" sui-icon suiBreadcrumbDivider></i>
+      <i suiIcon='right chevron' suiBreadcrumbDivider></i>
       <a suiBreadcrumbSection>Registration</a>
-      <i suiIconType="right arrow" sui-icon suiBreadcrumbDivider></i>
-      <div suiBreadcrumbSection suiActive='true'>Personal Information</div>
+      <i suiIcon='right arrow' suiBreadcrumbDivider></i>
+      <div suiBreadcrumbSection suiActive>Personal Information</div>
   </div>`;
 
   constructor(title: Title, private highlighter: HighlightService) {
