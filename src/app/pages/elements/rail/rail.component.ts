36cb20571c51fa305c3889f7c8b2c4df47b4d52c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-04-12 15:30:39 +0100

    #75 | add dividing docs

--- src/app/pages/elements/rail/rail.component.ts
+++ src/app/pages/elements/rail/rail.component.ts
@@ -8,7 +8,74 @@ import {HighlightService} from '../../../shared/services/highlight.service';
   styleUrls: ['./rail.component.scss']
 })
 export class RailComponent implements AfterViewChecked {
-  snippetBasic = ``;
+  snippetBasic = `
+  <div sui-grid
+       suiCentered
+       suiWidth="three">
+    <div suiGridColumn>
+      <div sui-segment>
+        <doc-wireframe type="paragraph"></doc-wireframe>
+
+        <div sui-rail
+             suiLocation="left">
+          <div sui-segment>
+            Left Rail Content
+          </div>
+        </div>
+        <div sui-rail
+             suiLocation="right">
+          <div sui-segment>
+            Right Rail Content
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  snippetInternal = `
+  <div sui-segment
+       sui-container>
+    <div sui-rail
+         suiInternal
+         suiLocation="left">
+      <div sui-segment>
+        Left Rail Content
+      </div>
+    </div>
+    <div sui-rail
+         suiInternal
+         suiLocation="right">
+      <div sui-segment>
+        Right Rail Content
+      </div>
+    </div>
+  </div>`;
+
+  snippetDividing = `
+  <div sui-grid
+       suiCentered
+       suiWidth="three">
+    <div suiGridColumn>
+      <div sui-segment>
+        <doc-wireframe type="paragraph"></doc-wireframe>
+
+        <div sui-rail
+             suiDividing
+             suiLocation="left">
+          <div sui-segment>
+            Left Rail Content
+          </div>
+        </div>
+        <div sui-rail
+             suiDividing
+             suiLocation="right">
+          <div sui-segment>
+            Right Rail Content
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
 
   constructor(title: Title, private highlighter: HighlightService) {
     title.setTitle('Rail | Ngx Semantic');
