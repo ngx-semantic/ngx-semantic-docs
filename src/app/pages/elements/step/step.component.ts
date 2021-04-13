d9494e6d03773cdc82529e0f5c979999f805771c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-04-13 23:18:57 +0100

    #78 | add page scaffold

--- src/app/pages/elements/step/step.component.ts
+++ src/app/pages/elements/step/step.component.ts
@@ -6,42 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step.component.scss']
 })
 export class StepComponent implements OnInit {
-  snippetSingleStep = `
+  snippetFade = `
   <div sui-steps>
     <div sui-step>
        Shipping
     </div>
   </div>
-  `;
-
-  snippetMultiStep = `
-  <div sui-steps>
-      <div sui-step
-           suiIcon="truck"
-           [suiContent]="truckContent">
-        <ng-template #truckContent>
-          <div class="title">Shipping</div>
-          <div class="description">Choose your shipping options</div>
-        </ng-template>
-      </div>
-      <div sui-step
-           suiIcon="payment"
-           suiActive
-           [suiContent]="paymentContent">
-        <ng-template #paymentContent>
-          <div class="title">Billing</div>
-          <div class="description">Enter billing information</div>
-        </ng-template>
-      </div>
-      <div sui-step
-           suiIcon="info"
-           [suiDisabled]
-           [suiContent]="orderContent">
-        <ng-template #orderContent>
-          <div class="title">Confirm Order</div>
-        </ng-template>
-      </div>
-    </div>
   `;
 
   constructor() { }
