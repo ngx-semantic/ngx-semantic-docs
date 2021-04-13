0344c618ab695d34865260e1f485809282b3ad39
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-04-13 23:44:01 +0100

    #78 | add set docs

--- src/app/pages/elements/step/step.component.ts
+++ src/app/pages/elements/step/step.component.ts
@@ -15,6 +15,35 @@ export class StepComponent implements AfterViewChecked {
     </div>
   </div>`;
 
+  snippetSet = `
+  <div sui-steps>
+    <div sui-step>
+      <i sui-icon
+         suiIconType="truck"></i>
+      <div suiStepContent>
+        <div suiStepTitle>Shipping</div>
+        <div suiStepDescription>Choose your shipping options</div>
+      </div>
+    </div>
+    <div sui-step
+         suiActive>
+      <i sui-icon
+         suiIconType="payment"></i>
+      <div suiStepContent>
+        <div suiStepTitle>Billing</div>
+        <div suiStepDescription>Enter billing information</div>
+      </div>
+    </div>
+    <div sui-step
+         disabled>
+      <i sui-icon
+         suiIconType="info"></i>
+      <div suiStepContent>
+        <div suiStepTitle>Confirm Order</div>
+      </div>
+    </div>
+  </div>`;
+
   constructor(title: Title, private highlighter: HighlightService) {
     title.setTitle('Step | Ngx Semantic');
   }
