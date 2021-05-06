f99cdd866350b3f7b681a5c22fc1b80f9199655f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-06 13:04:48 +0100

    #20 | add basic image docs

--- src/app/pages/elements/image/image.component.ts
+++ src/app/pages/elements/image/image.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {HighlightService} from '../../../shared/services/highlight.service';
 
 @Component({
   selector: 'doc-image',
-  template: `
-    <p>
-      image works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './image.component.html',
+  styles: []
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent implements AfterViewChecked {
+  snippetBasic = `
+  <img sui-image
+     suiSize="small"
+     src="https://semantic-ui.com/images/wireframe/image.png">`;
 
-  constructor() { }
+  snippetBasicInnerImg = `
+  <div sui-image
+       suiSize="small">
+    <img src="https://semantic-ui.com/images/wireframe/image.png">
+  </div>`;
 
-  ngOnInit(): void {
+  snippetBasicInnerSvg = `
+  <div sui-image
+       suiSize="small">
+    <svg width="150" height="150">
+      <image xlink:href="https://semantic-ui.com/images/wireframe/image.png" x="0" y="0" width="100%" height="100%"></image>
+    </svg>
+  </div>`;
+
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Input | Ngx Semantic');
   }
 
+  ngAfterViewChecked() {
+    this.highlighter.highlightAll();
+  }
 }
