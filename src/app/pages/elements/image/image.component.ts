41a7199c175d8a2936b3bcc948b09b123992430c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-08 23:50:21 +0100

    #20 | add image centered docs

--- src/app/pages/elements/image/image.component.ts
+++ src/app/pages/elements/image/image.component.ts
@@ -93,6 +93,28 @@ export class ImageComponent implements AfterViewChecked {
        src="https://semantic-ui.com/images/wireframe/square-image.png">
   <span>Bottom Aligned</span>`;
 
+  snippetCentered = `
+  <div sui-segment>
+    <img sui-image
+         suiCentered
+         suiSize="medium"
+         src="https://semantic-ui.com/images/wireframe/image.png">
+    <p>Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem, est autem
+      aliquip detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque
+      electram, eos choro alterum definiebas in. Vim dolorum definiebas an. Mei ex natum rebum iisque.</p>
+    <p>Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi nulla ex duo.
+      Vis sint solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix fabulas oporteat, ei quo vide
+      phaedrum, vim vivendum maiestatis in.</p>
+    <img sui-image
+         suiCentered
+         suiSize="small"
+         src="https://semantic-ui.com/images/wireframe/text-image.png">
+    <p>Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores adolescens, no
+      illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo. Populo quodsi te
+      eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu per, quas minimum
+      postulant per id.</p>
+  </div>`;
+
   constructor(title: Title, private highlighter: HighlightService) {
     title.setTitle('Image | Ngx Semantic');
   }
