13f870c3ab5f8bc9516aaf607a2be90f65e29d2f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-09 18:37:37 +0100

    #20 | use base page for the flags

--- src/app/pages/elements/flag/flag.component.ts
+++ src/app/pages/elements/flag/flag.component.ts
@@ -1,24 +1,22 @@
-import {AfterViewChecked, Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {HighlightService} from '../../../shared/services/highlight.service';
+import {BasePageComponent} from '../../../shared/components/base-page.component';
 
 @Component({
   selector: 'doc-flags',
   templateUrl: './flag.component.html',
   styleUrls: ['./flag.component.scss']
 })
-export class FlagComponent implements AfterViewChecked {
+export class FlagComponent extends BasePageComponent {
   snippetFlag = `
   <i sui-flag suiCountry="ae"></i>
   <i sui-flag suiCountry="nigeria"></i>
   <i sui-flag suiCountry="burkina faso"></i>
   <i sui-flag suiCountry="czech republic"></i>`;
 
-  constructor(title: Title, private highlighter: HighlightService) {
+  constructor(title: Title, highlighter: HighlightService) {
+    super(highlighter);
     title.setTitle('Flag | Ngx Semantic');
-  }
-
-  ngAfterViewChecked() {
-    this.highlighter.highlightAll();
   }
 }
--- src/app/shared/components/base-page.component.ts
+++ src/app/shared/components/base-page.component.ts
@@ -1,9 +1,12 @@
 /**
  * Created by bolorundurowb on 5/9/2021
  */
-import {AfterViewChecked} from '@angular/core';
+import {AfterViewChecked, Directive} from '@angular/core';
 import {HighlightService} from '../services/highlight.service';
 
+@Directive({
+  selector: 'doc-base-page'
+})
 export abstract class BasePageComponent implements AfterViewChecked {
   protected constructor(private highlighter: HighlightService) {
   }
