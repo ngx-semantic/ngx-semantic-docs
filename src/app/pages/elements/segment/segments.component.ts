c8a5430fd100f9154a748129ad229a82c17a3bef
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-15 22:51:03 +0100

    #77 | inherit right

--- src/app/pages/elements/segment/segments.component.ts
+++ src/app/pages/elements/segment/segments.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BasePageComponent} from '../../../shared/components/base-page.component';
+import {HighlightService} from '../../../shared/services/highlight.service';
 
 @Component({
   selector: 'doc-segments',
   templateUrl: './segments.component.html',
   styleUrls: ['./segments.component.scss'],
 })
-export class SegmentsComponent implements OnInit {
+export class SegmentsComponent extends BasePageComponent {
   snippetSegment = `
   <div sui-segment suiColour="red" [suiInverted]>Red</div>
   <div sui-segment suiColour="orange" [suiInverted]>Orange</div>
@@ -13,7 +15,7 @@ export class SegmentsComponent implements OnInit {
   <div sui-segment suiColour="olive" [suiInverted]>Olive</div>
   <div sui-segment suiColour="green" [suiInverted]>Green</div>`;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(highlighter: HighlightService) {
+    super(highlighter);
+  }
 }
