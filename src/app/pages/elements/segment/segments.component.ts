e8e95897b59300dd4ed5ab223f6ad27a3a30cf52
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-16 08:26:08 +0100

    #77 | add stacked docs

--- src/app/pages/elements/segment/segments.component.ts
+++ src/app/pages/elements/segment/segments.component.ts
@@ -97,6 +97,14 @@ export class SegmentsComponent extends BasePageComponent {
       semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
   </div>`;
 
+  snippetStacked = `
+  <div sui-segment
+       suiStacked="stacked">
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+      tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas
+      semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+  </div>`;
+
   constructor(title: Title, highlighter: HighlightService) {
     super(highlighter);
     title.setTitle('Segment | Ngx Semantic');
