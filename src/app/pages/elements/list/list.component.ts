15a930afadbc8c63259a450b17ff6dec9f6ba39b
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-05-17 20:21:48 +0100

    update docs

--- src/app/pages/elements/list/list.component.ts
+++ src/app/pages/elements/list/list.component.ts
@@ -7,32 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
   snippetList = `
-    <div  sui-list>
-      <div class="item">
-        <i sui-icon suiIconType="users"></i>
-        <div class="content">
-          Semantic UI
-        </div>
-      </div>
-      <div class="item">
-        <i sui-icon suiIconType="marker"></i>
-        <div class="content">
-          New York, NY
-        </div>
-      </div>
-      <div class="item">
-        <i sui-icon suiIconType="mail"></i>
-        <div class="content">
-          <a href="mailto:jack@semantic-ui.com">jack@semantic-ui.com</a>
-        </div>
-      </div>
-      <div class="item">
-        <i sui-icon suiIconType="linkify"></i>
-        <div class="content">
-          <a href="http://www.semantic-ui.com">semantic-ui.com</a>
-        </div>
+<div  sui-list>
+  <div suiListItem>
+    Apples
+  </div>
+  <div suiListItem>
+    Pears
+  </div>
+  <div suiListItem>
+    Oranges
+  </div>
+</div>
+  `;
+  snippetList1 = `
+  <div  sui-list>
+    <div suiListItem>
+      <i sui-icon suiIconType="users"></i>
+      <div suiListContent>
+        Semantic UI
       </div>
     </div>
+    <div suiListItem>
+      <i sui-icon suiIconType="marker"></i>
+      <div suiListContent>
+        New York, NY
+      </div>
+    </div>
+    <div suiListItem>
+      <i sui-icon suiIconType="mail"></i>
+      <div suiListContent>
+        <a href="mailto:jack@semantic-ui.com">jack@semantic-ui.com</a>
+      </div>
+    </div>
+    <div suiListItem>
+      <i sui-icon suiIconType="linkify"></i>
+      <div suiListContent>
+        <a href="http://www.semantic-ui.com">semantic-ui.com</a>
+      </div>
+    </div>
+  </div>
+  `;
+  snippetList2= `
+  <div sui-list suiDivided="true" SuiListRelaxation="relaxed">
+    <div suiListItem>
+      <i sui-icon suiIconType="github" suiSize="large"></i>
+      <div suiListContent>
+        <a sui-header suiSize="small">Semantic-Org/Semantic-UI</a>
+        <div suiListDescription>Updated 10 mins ago</div>
+      </div>
+    </div>
+    <div suiListItem>
+      <i sui-icon suiIconType="github" suiSize="large"></i>
+      <div suiListContent>
+        <a sui-header suiSize="small">Semantic-Org/Semantic-UI-Docs</a>
+        <div suiListDescription>Updated 22 mins ago</div>
+      </div>
+    </div>
+    <div suiListItem>
+      <i sui-icon suiIconType="github" suiSize="large"></i>
+      <div suiListContent>
+        <a sui-header suiSize="small">Semantic-Org/Semantic-UI-Meteor</a>
+        <div suiListDescription>Updated 34 mins ago</div>
+      </div>
+    </div>
+  </div>
   `;
 
   constructor() { }
