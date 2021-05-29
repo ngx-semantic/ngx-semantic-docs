ce00072551a56ba1625fffa84f8bac0f99a05f43
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-29 20:38:59 +0100

    #6 | add pages

--- src/app/pages/general/contributing/contributing.component.ts
+++ src/app/pages/general/contributing/contributing.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'doc-contributing',
   templateUrl: './contributing.component.html'
 })
 export class ContributingComponent {
+  constructor(title: Title) {
+    title.setTitle('Contributing | Ngx Semantic')
+  }
 }
