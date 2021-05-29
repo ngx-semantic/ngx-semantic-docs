a9c9f44e77b1eb13279feeee6c4e3357b3380a34
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-29 20:27:15 +0100

    #6 | add page scaffold

---
+++ src/app/pages/general/contributing/contributing.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'doc-contributing',
+  template: `
+    <p>
+      contributing works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ContributingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
