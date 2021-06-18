c2a89af99c6b30338851b11683e9b729e3e000b4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-18 08:31:35 +0100

    #58 | add the success docs

--- src/app/pages/collections/form/form.component.ts
+++ src/app/pages/collections/form/form.component.ts
@@ -280,6 +280,19 @@ export class FormComponent extends BasePageComponent {
     <div sui-button>Submit</div>
   </div>`;
 
+  snippetSuccess = `
+  <div sui-form suiState="success">
+    <div suiFormField>
+      <label>E-mail</label>
+      <input type="email" placeholder="joe@schmoe.com">
+    </div>
+    <div sui-message suiState="success">
+      <div suiMessageHeader>Form Completed</div>
+      <p>You're all signed up for the newsletter.</p>
+    </div>
+    <div sui-button>Submit</div>
+  </div>`;
+
   constructor(title: Title, highlighter: HighlightService) {
     super(highlighter);
     title.setTitle('Form | Ngx Semantic');
