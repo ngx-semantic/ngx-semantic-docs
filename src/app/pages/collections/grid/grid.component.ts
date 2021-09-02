293a46ebb30d71330f0efd0314c0dcff40c9194d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-09-03 00:38:47 +0300

    #59 | add the grid component title

--- src/app/pages/collections/grid/grid.component.ts
+++ src/app/pages/collections/grid/grid.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {HighlightService} from "../../../shared/services/highlight.service";
+import {BasePageComponent} from "../../../shared/components/base-page.component";
 
 @Component({
   selector: 'doc-grid',
-  templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss'],
+  templateUrl: './grid.component.html'
 })
-export class GridComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class GridComponent extends BasePageComponent {
+  constructor(title: Title, highlighter: HighlightService) {
+    super(highlighter);
+    title.setTitle('Grid | Ngx Semantic');
   }
-
 }
