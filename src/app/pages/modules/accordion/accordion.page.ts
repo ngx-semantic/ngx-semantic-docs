80f90e45146a948bb5069a18d2b8427a5476a981
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-04-22 11:51:52 +0100

    #41 | add page basic scaffold

--- src/app/pages/modules/accordion/accordion.page.ts
+++ src/app/pages/modules/accordion/accordion.page.ts
@@ -1,10 +1,18 @@
 import { AfterContentChecked, Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { HighlightService } from '../../../shared/services/highlight.service';
 
 @Component({
   selector: 'doc-accordion',
   templateUrl: './accordion.page.html'
 })
 export class AccordionPage implements AfterContentChecked {
+
+  constructor(title: Title, private highlighter: HighlightService) {
+    title.setTitle('Accordion | Ngx Semantic');
+  }
+
   ngAfterContentChecked() {
+    this.highlighter.highlightAll();
   }
 }
