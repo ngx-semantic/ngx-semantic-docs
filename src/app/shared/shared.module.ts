b1b62b3425f92c6f7021425d3f621d4fb4964d9e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-04-27 11:37:23 +0100

    #139 | add in providers for the highlighter

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,23 +2,24 @@
  * Created by bolorundurowb on 1/28/2021
  */
 
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {SuiIconModule} from "ngx-semantic/elements/icon";
-import {SuiPopupModule} from "ngx-semantic/modules/popup";
-import {SuiLabelModule} from "ngx-semantic/elements/label";
-import {SuiGridModule} from "ngx-semantic/collections/grid";
-import {SuiMenuModule} from "ngx-semantic/collections/menu";
-import {SuiHeaderModule} from "ngx-semantic/elements/header";
-import {SuiDividerModule} from "ngx-semantic/elements/divider";
-import {SuiSegmentModule} from "ngx-semantic/elements/segment";
-import {SuiContainerModule} from "ngx-semantic/elements/container";
+import { SuiIconModule } from 'ngx-semantic/elements/icon';
+import { SuiPopupModule } from 'ngx-semantic/modules/popup';
+import { SuiLabelModule } from 'ngx-semantic/elements/label';
+import { SuiGridModule } from 'ngx-semantic/collections/grid';
+import { SuiMenuModule } from 'ngx-semantic/collections/menu';
+import { SuiHeaderModule } from 'ngx-semantic/elements/header';
+import { SuiDividerModule } from 'ngx-semantic/elements/divider';
+import { SuiSegmentModule } from 'ngx-semantic/elements/segment';
+import { SuiContainerModule } from 'ngx-semantic/elements/container';
 
-import {DocPageComponent} from "./components/doc-page.component";
-import {CodeSampleComponent} from "./components/code-sample.component";
-import {DocPageContentDirective} from './directives/doc-page-content.directive';
-import {DocWireframeComponent} from './components/doc-wireframe.component';
+import { DocPageComponent } from './components/doc-page.component';
+import { CodeSampleComponent } from './components/code-sample.component';
+import { DocPageContentDirective } from './directives/doc-page-content.directive';
+import { DocWireframeComponent } from './components/doc-wireframe.component';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 
 @NgModule({
   imports: [
@@ -44,7 +45,13 @@ import {DocWireframeComponent} from './components/doc-wireframe.component';
     DocPageComponent,
     DocPageContentDirective,
     DocWireframeComponent
-  ]
+  ],
+  providers: [
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js'),
+      lineNumbersLoader: () => import('ngx-highlightjs/line-numbers')
+    }),
+  ],
 })
 export class SharedModule {
 }
