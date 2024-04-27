7ed23022980292f595f5329f3d9ce6a7e3d1f8df
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-04-27 20:38:25 +0100

    #66 | add feed page routing

--- src/app/pages/views/views.module.ts
+++ src/app/pages/views/views.module.ts
@@ -2,17 +2,19 @@
  * Created by bolorundurowb on 1/28/2021
  */
 
-import {NgModule} from "@angular/core";
-import {CardsComponent} from "./card/cards.component";
-import {CommonModule} from "@angular/common";
-import {ViewsRoutingModule} from "./views-routing.module";
-import {SharedModule} from "../../shared/shared.module";
-import {NgxSemanticModule} from "ngx-semantic";
-import {StatisticsComponent} from './statistic/statistics.component';
+import { NgModule } from '@angular/core';
+import { CardsComponent } from './card/cards.component';
+import { CommonModule } from '@angular/common';
+import { ViewsRoutingModule } from './views-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { NgxSemanticModule } from 'ngx-semantic';
+import { StatisticsComponent } from './statistic/statistics.component';
+import { FeedPage } from './feed/feed.page';
 
 @NgModule({
   declarations: [
     CardsComponent,
+    FeedPage,
     StatisticsComponent
   ],
   imports: [
