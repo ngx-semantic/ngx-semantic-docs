669e4a76e8b16b4004511fe860b5d27a6565716f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-01 16:53:39 +0100

    #42 | add page layout

--- src/app/pages/modules/checkbox/checkbox.page.ts
+++ src/app/pages/modules/checkbox/checkbox.page.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'doc-checkbox',
   templateUrl: 'checkbox.page.html'
 })
-export class CheckboxPage {}
+export class CheckboxPage {
+  constructor(title: Title) {
+    title.setTitle('Checkbox | Ngx Semantic');
+  }
+}
