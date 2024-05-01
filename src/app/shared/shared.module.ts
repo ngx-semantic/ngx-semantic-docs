c5434393081104b6142bdbe479e944d0addd1470
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-01 14:38:08 +0100

    #66 | fix the colour highluights

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -51,12 +51,8 @@ import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
   ],
   providers: [
     provideHighlightOptions({
-      coreLibraryLoader: () => import('highlight.js/lib/core'),
-      languages: {
-        typescript: () => import('highlight.js/lib/languages/typescript'),
-        css: () => import('highlight.js/lib/languages/css'),
-        html: () => import('highlight.js/lib/languages/vbscript-html')
-      },
+      fullLibraryLoader: () => import('highlight.js'),
+      lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
     }),
   ],
 })
