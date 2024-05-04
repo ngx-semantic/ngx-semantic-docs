04b0474deaa7780cc6964c22574b70b8f454aa33
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-04 17:39:17 +0100

    #63 | finalize types documentation

--- src/app/pages/views/advertisement/advertisement.page.ts
+++ src/app/pages/views/advertisement/advertisement.page.ts
@@ -56,6 +56,32 @@ export class AdvertisementPage {
   <div sui-advertisement suiTest suiMobile suiType="leaderboard" data-text="Mobile Leaderboard"></div>
   <div sui-advertisement suiTest suiMobile suiType="banner" data-text="Mobile Banner"></div>`;
 
+  snippetButton = `
+  <div sui-advertisement suiTest suiType="button" data-text="Button"></div>
+  <div sui-advertisement suiTest suiSquare suiType="button" data-text="Square Button"></div>
+  <div sui-advertisement suiTest suiSize="small" suiType="button" data-text="Small Button"></div>`;
+
+  snippetSkyscraper = `
+  <div sui-advertisement suiTest suiType="skyscraper" data-text="Skyscraper"></div>
+  <div sui-advertisement suiTest suiWide suiType="skyscraper" data-text="Wide Skyscraper"></div>`;
+
+  snippetBanner = `
+  <div sui-advertisement suiTest suiType="banner" data-text="Banner"></div>
+  <div sui-advertisement suiTest suiVertical suiType="banner" data-text="Vertical Banner"></div>
+  <div sui-advertisement suiTest suiType="banner" data-text="Top Banner"></div>
+  <div sui-advertisement suiTest suiHalf suiType="banner" data-text="Half Banner"></div>`;
+
+  snippetLeaderboard = `
+  <div sui-advertisement suiTest suiType="leaderboard" data-text="Leaderboard"></div>
+  <div sui-advertisement suiTest suiSize="large" suiType="leaderboard" data-text="Large Leaderboard"></div>
+  <div sui-advertisement suiTest suiType="billboard" data-text="Billboard"></div>`;
+
+  snippetPanorama = `
+  <div sui-advertisement suiTest suiType="panorama" data-text="Panorama"></div>`;
+
+  snippetNetboard = `
+  <div sui-advertisement suiTest suiType="netboard"></div>`;
+
   constructor(title: Title) {
     title.setTitle('Advertisement | Ngx Semantic');
   }
