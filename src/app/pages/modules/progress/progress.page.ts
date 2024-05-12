0e16384fc71f88eca892825466503f028f0f680f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-12 23:32:17 +0100

    #48 | add example code

--- src/app/pages/modules/progress/progress.page.ts
+++ src/app/pages/modules/progress/progress.page.ts
@@ -6,7 +6,67 @@ import { Title } from '@angular/platform-browser';
   templateUrl: 'progress.page.html'
 })
 export class ProgressPage {
-  snippetStandard = ``;
+  snippetStandard = `
+  <sui-progress
+      suiShowProgress
+      [suiValue]="31">
+    Uploading Files
+  </sui-progress>`;
+
+  snippetIndicating = `
+  <sui-progress
+      suiIndicating
+      [suiValue]="indicatingValue">
+    {{indicatingValue}}% Funded
+  </sui-progress>`;
+
+  snippetIndicatingTs = `
+  indicatingValue = 40;`;
+
+  snippetBar = `
+  <sui-progress
+      [suiValue]="28">
+  </sui-progress>`;
+
+  snippetProgress = `
+  <sui-progress
+      suiShowProgress
+      [suiValue]="35">
+  </sui-progress>`;
+
+  snippetActive = `
+  <sui-progress
+      suiState="active"
+      [suiValue]="51">
+    Uploading Files
+  </sui-progress>`;
+
+  snippetSuccess = `
+  <sui-progress
+      suiState="success"
+      [suiValue]="100">
+    Everything worked, your file is all ready.
+  </sui-progress>`;
+
+  snippetWarning = `
+  <sui-progress
+      suiState="warning"
+      [suiValue]="100">
+    Your file didn't meet the minimum resolution requirements.
+  </sui-progress>`;
+
+  snippetError = `
+  <sui-progress
+      suiState="error"
+      [suiValue]="100">
+    There was an error.
+  </sui-progress>`;
+
+  snippetDisabled = `
+  <sui-progress
+      disabled
+      [suiValue]="38">
+  </sui-progress>`;
 
   standardValue = 31;
   indicatingValue = 40;
@@ -15,7 +75,7 @@ export class ProgressPage {
     title.setTitle('Progress | Ngx Semantic');
   }
 
-  addToStandard(value: number) {
+  addToStandard(value: number): void {
     let computedValue = this.standardValue + value;
 
     if (computedValue > 100) {
@@ -25,5 +85,17 @@ export class ProgressPage {
     }
 
     this.standardValue = computedValue;
+  }
+
+  addToIndicating(value: number): void {
+    let computedValue = this.indicatingValue + value;
+
+    if (computedValue > 100) {
+      computedValue = 100;
+    } else if (computedValue < 0) {
+      computedValue = 0;
+    }
+
+    this.indicatingValue = computedValue;
   }
 }
