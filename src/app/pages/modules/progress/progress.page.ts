7ce473a6329efdb21a4202fb3e0fa5100981beb9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-12 22:07:13 +0100

    #48 | add some ui elements

--- src/app/pages/modules/progress/progress.page.ts
+++ src/app/pages/modules/progress/progress.page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'doc-progress',
   templateUrl: 'progress.page.html'
 })
 export class ProgressPage {
+  snippetStandard = ``;
+
+  standardValue = 31;
+  indicatingValue = 40;
+
+  constructor(title: Title) {
+    title.setTitle('Progress | Ngx Semantic');
+  }
+
+  addToStandard(value: number) {
+    let computedValue = this.standardValue + value;
+
+    if (computedValue > 100) {
+      computedValue = 100;
+    } else if (computedValue < 0) {
+      computedValue = 0;
+    }
+
+    this.standardValue = computedValue;
+  }
 }
