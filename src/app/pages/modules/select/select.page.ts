8debab59d3107db3bd74de3e2bbea7e71f40a80a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-18 23:28:46 +0100

    #51 | add api docs

--- src/app/pages/modules/select/select.page.ts
+++ src/app/pages/modules/select/select.page.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'doc-select',
   templateUrl: 'select.page.html'
 })
-export class SelectPage{}
+export class SelectPage {
+  constructor(title: Title) {
+    title.setTitle('Select | Ngx Semantic');
+  }
+}
